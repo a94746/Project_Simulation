8a19641955de62376b57b66de99fa42178431a81
dmitry <dmitryed6263@gmail.com>
2025-02-11 14:48:40 +0400

    Initial commit

--- src/main/java/org/project_simulation/Main.java
+++ src/main/java/org/project_simulation/Main.java
@@ -15,29 +15,30 @@ public class Main{
         InitActions initActions = new InitActions(gameMap);
         initActions.initGameMap();
         Renderer renderer = new Renderer(gameMap);
-
         renderer.render();
-        int count1 = 0;
-        int count2 = 0;
-        int count3 = 0;
-        int count4 = 0;
-        int count5 = 0;
-        for (Entity c : gameMap.getEntities().values()) {
-            if (c instanceof Grass) {
-                count1++;
-            }
-            else if (c instanceof Tree){
-                count2++;
-            } else if (c instanceof Rock){
-                count3++;
-            } else if (c instanceof Herbivore){
-                count4++;
-            } else if (c instanceof Predator){
-                count5++;
-            }
-        }
-        System.out.println(count1 + " Grass " + count2 + "  Tree " + count3 + " Herbivore " + count4 + " Predator " + count5);
-        System.out.println( count5 + count1 + count2 + count3 + count4  );
+//        int count1 = 0;
+//        int count2 = 0;
+//        int count3 = 0;
+//        int count4 = 0;
+//        int count5 = 0;
+//        for (Entity c : gameMap.getEntities().values()) {
+//            if (c instanceof Grass) {
+//                count1++;
+//            }
+//            else if (c instanceof Tree){
+//                count2++;
+//            } else if (c instanceof Rock){
+//                count3++;
+//            } else if (c instanceof Herbivore){
+//                count4++;
+//            } else if (c instanceof Predator){
+//                count5++;
+//            }
+//        }
+//        System.out.println(count1 + " Grass " + count2 + "  Tree " + count3 + " Herbivore " + count4 + " Predator " + count5);
+//        System.out.println( count5 + count1 + count2 + count3 + count4  );
+
+
 
 
 
